80a6114b7c185af42e56f4afe4d31e07ed6c2636
Kirill <1c81@mail.ru>
2020-03-12 10:00:38 +0300

    Инициализация start.spring.io

---
+++ src/main/java/ru/kolyasnikovkv/collectiondvd20/Collectiondvd20Application.java
@@ -0,0 +1,13 @@
+package ru.kolyasnikovkv.collectiondvd20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Collectiondvd20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Collectiondvd20Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kolyasnikovkv/collectiondvd20/Collectiondvd20ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kolyasnikovkv.collectiondvd20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Collectiondvd20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
