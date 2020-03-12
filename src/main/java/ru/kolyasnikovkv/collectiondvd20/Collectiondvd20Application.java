8915f82fa84583f3ce7384b8cda59c9c78157d20
Kirill <1c81@mail.ru>
2020-03-12 14:07:39 +0300

    Инициализация start.spring.io

--- src/main/java/ru/kolyasnikovkv/collectiondvd20/Collectiondvd20Application.java
+++ src/main/java/ru/kolyasnikovkv/collectiondvd20/Collectiondvd20Application.java
@@ -1,11 +1,19 @@
 package ru.kolyasnikovkv.collectiondvd20;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Collectiondvd20Application {
 
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Collectiondvd20Application.class, args);
 	}
